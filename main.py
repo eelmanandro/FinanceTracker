ff62551f66e40b5464df1c9d37be936e7d7e2747
eelmanandro <eelmanandro@gmail.com>
2026-05-30 11:49:43 +0200

    Implement methods for saving, load, add transactions.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout,
 from PyQt5.QtGui import QDoubleValidator
 from PyQt5.QtCore import Qt, QDate
 import sys
+import json
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -49,26 +50,69 @@ class MainWindow(QMainWindow):
         col2.addWidget(self.transaction_description_label)
         col2.addWidget(self.transaction_description)
         col2.addWidget(self.add_button)
-        self.add_button.clicked.connect(self.get_data)
+        self.add_button.clicked.connect(self.add_transaction)
 
         # Add columns to main layout
         layout.addLayout(col1, stretch = 70)
         layout.addLayout(col2, stretch=30)
 
+        self.transactions_filename = "transactions.json"
+        self.transactions = self.load_transactions()
+
     def get_data(self):
         transaction_type = self.transaction_type.currentText()
         amount = self.amount_input.text()
         category = self.transaction_category.text()
-        date = self.transaction_date.selectedDate()
+        date = self.transaction_date.selectedDate().toString("yyyy-MM-dd")
         description = self.transaction_description.toPlainText()
-        print( {
+        return{
             "type": transaction_type,
             "amount": amount,
             "category": category,
             "date": date,
             "description": description
-        })
+        }
     
+    def load_transactions(self):
+        try:
+            with open(self.transactions_filename, "r") as file:
+                transactions = json.load(file)
+            return transactions
+        except FileNotFoundError:
+            transactions = {}
+            with open(self.transactions_filename, "w") as file:
+                json.dump(transactions, file, ensure_ascii=False, indent=2)
+            return transactions
+
+    def get_lastID(self):
+        IDs = list(map(int, self.transactions.keys()))
+        if IDs:
+            return IDs[-1]
+        else:
+            return 0
+
+    def add_transaction(self):
+        transaction_data = self.get_data()
+        last_id = self.get_lastID()
+        self.transactions[str(last_id + 1)] = transaction_data
+        self.transactions = dict(self.sort_transactions())
+        with open(self.transactions_filename, "w") as file:
+            json.dump(self.transactions, file, ensure_ascii=False, indent=2)
+        self.clear_inputs()
+
+    def sort_transactions(self):
+        def get_num_key(transaction):
+            return int(transaction[0])
+        sorted_transactions = sorted(self.transactions.items(), key=get_num_key)
+        return sorted_transactions
+    
+    def clear_inputs(self):
+        self.amount_input.clear()
+        self.transaction_category.clear()
+        self.transaction_description.clear()
+        self.transaction_date.setSelectedDate(QDate.currentDate())
+    
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
